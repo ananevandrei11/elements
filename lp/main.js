55c35d4fd7460248b516d86466f5fcb55614f536
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2022-07-10 21:50:31 +0200

    Pretty code

--- lp/main.js
+++ lp/main.js
@@ -38,33 +38,33 @@ gsap.from('.nav__item', {
   duration: 1,
   delay: 2.1,
   y: 30,
-	stagger: 0.2
+  stagger: 0.2,
 });
 
 gsap.from('.home__title', {
   opacity: 0,
   duration: 1,
   delay: 1.6,
-  y: 30
+  y: 30,
 });
 
 gsap.from('.home__description', {
   opacity: 0,
   duration: 1,
   delay: 1.8,
-  y: 30
+  y: 30,
 });
 
 gsap.from('.home__button', {
   opacity: 0,
   duration: 1,
   delay: 2.1,
-  y: 30
+  y: 30,
 });
 
 gsap.from('.home__img', {
   opacity: 0,
   duration: 1,
   delay: 1.3,
-  y: 30
+  y: 30,
 });
